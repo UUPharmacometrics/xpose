ecec70dcdc2de9ab8ece5b3fb87f9cec5249fb15
Bill Denney <wdenney@humanpredictions.com>
2019-01-17 20:12:23 -0500

    Allow assignment into xpose_data class without conversion to uneval

--- R/xpose_data.R
+++ R/xpose_data.R
@@ -171,4 +171,10 @@ xpose_data <- function(runno         = NULL,
     structure(class = c('xpose_data', 'uneval'))
 }
 
-
+#' Allow assignment into xpose_data without converting to class uneval
+`[[<-.xpose_data` <- function(x, i, value) {
+  x <- unclass(x)
+  x[[i]] <- value
+  as.xpdb(x)
+}
+`$<-.xpose_data` <- `[[<-.xpose_data`
